a5f15e982509644a3c6dd0f3691a0a39c2835626
Craig Bowditch <craigbowditch@hotmail.com>
2018-09-25 16:08:44 +0100

    Adds count number of books method.

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    Library library;
+    Book book;
+    
+    
+    @Before
+    public void before(){
+        library = new Library();
+        book = new Book("Catcher in the rye", "JD Salinger", "Wanky");
+    }
+
+    @Test
+    public void canCountNumberOfBooks() {
+        assertEquals(0, library.bookCount());
+    }
 }
